93fb7e7fa7613c5ee558ba6b9a4bbc23627080d7
Nathan Mielcarek <nathan.l.mielcarek.ctr@mail.mil>
2020-05-05 17:25:54 -0700

    Added hide mouse cursor command.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+// hide the mouse cursor
+    QApplication::setOverrideCursor(Qt::BlankCursor);
 
     QCommandLineParser parser;
     parser.addHelpOption();
